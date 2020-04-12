2f22fbb92757acb00e3c23cdd6aabf993e24f75b
otaviojava <otaviopolianasantana@gmail.com>
2020-04-12 14:55:41 -0300

    initial project

---
+++ spring-mongodb/src/main/java/sh/platform/template/Person.java
@@ -0,0 +1,67 @@
+package sh.platform.template;
+
+import org.springframework.data.annotation.Id;
+
+import java.util.Objects;
+
+
+public class Person {
+
+    @Id
+    private String id;
+
+    private String name;
+
+    private Integer age;
+
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Person person = (Person) o;
+        return Objects.equals(id, person.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
+}
